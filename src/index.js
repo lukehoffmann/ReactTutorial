709d3cceb52e617eb3c5b5f2e088e92e997879d3
Luke Hoffmann <luke.hoffmann@optimatics.com>
2020-05-10 12:59:22 +0930

    raise game state to Game component

--- src/index.js
+++ src/index.js
@@ -15,48 +15,17 @@ function Square (props) {
  * The tic-tac-toe board
  */
 class Board extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      moves: Array(9).fill(null),
-      nextMove: 'X',
-      winner: null
-    }
-  }
-
   renderSquare (i) {
     return <Square
-      move={this.state.moves[i]}
-      onClick={() => this.handleClick(i)}
+      move={this.props.moves[i]}
+      onClick={() => this.props.handleClick(i)}
     />
   }
 
-  handleClick (i) {
-    // is the game already over?
-    if (this.state.winner) return
-
-    let nextMove = this.state.nextMove
-    const moves = this.state.moves.slice()
-
-    // is the square already occupied?
-    if (moves[i]) return
-
-    // otherwise make the move and update the game state
-    moves[i] = nextMove
-    this.setState({moves: moves})
-    this.setState({nextMove: nextMove === 'X' ? 'O' : 'X'})
-    this.setState({winner: this.calculateWinner(moves)})
-  }
-
   render () {
-    const status =
-      (this.state.winner)
-        ? `Winner: ${this.state.winner}`
-        : `Next player: ${this.state.nextMove}`
-
     return (
       <div>
-        <div className='status'>{status}</div>
+        <div className='status'>{this.props.status}</div>
         <div className='board-row'>
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -75,8 +44,67 @@ class Board extends React.Component {
       </div>
     )
   }
+}
+
+class Game extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      history: [{
+        moves: Array(9).fill(null),
+        nextMove: 'X',
+        winner: null
+      }]
+    }
+  }
+
+  handleClick (i) {
+    const [current] = this.state.history.slice(-1)
+    // is the game already over?
+    if (current.winner) return
+
+    let nextMove = current.nextMove
+    const moves = current.moves.slice()
+
+    // is the square already occupied?
+    if (moves[i]) return
+
+    // otherwise make the move and add the game state to history
+    moves[i] = nextMove
+    this.setState({history: this.state.history.concat([
+      {
+        moves: moves,
+        nextMove: nextMove === 'X' ? 'O' : 'X',
+        winner: this.calculateWinner(moves)
+      }
+    ])})
+  }
+
+  render () {
+    const [current] = this.state.history.slice(-1)
+    const status =
+      (current.winner)
+        ? `Winner: ${current.winner}`
+        : `Next player: ${current.nextMove}`
+
+    return (
+      <div className='game'>
+        <div className='game-board'>
+          <Board
+            moves={current.moves}
+            status={status}
+            handleClick={(i) => this.handleClick(i)} />
+        </div>
+        <div className='game-info'>
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    )
+  }
 
   calculateWinner (moves) {
+    // we can just check all of the known winning combos
     const winStates = [
       [0, 1, 2],
       [3, 4, 5],
@@ -93,22 +121,6 @@ class Board extends React.Component {
       }
     }
     return null
-  }
-}
-
-class Game extends React.Component {
-  render () {
-    return (
-      <div className='game'>
-        <div className='game-board'>
-          <Board />
-        </div>
-        <div className='game-info'>
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    )
   }
 }
 
